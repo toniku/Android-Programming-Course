d61a3f452778fadb0ff3411bbd627bb13c5cd41c
Toni <K>
2019-02-15 15:38:29 +0200

    Simple timer app
    Countdown timer app

---
+++ Lab3.4/app/src/main/java/com/example/egg_timer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.egg_timer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static String EXTRA_MESSAGE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.startButton).setOnClickListener(this);
+        EditText timeInSeconds = findViewById(R.id.timeField);
+        timeInSeconds.setSelection(timeInSeconds.getText().length());
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.startButton) {
+            Intent intent = new Intent(this, TimerActivity.class);
+            EditText timeInSeconds = findViewById(R.id.timeField);
+            String time = timeInSeconds.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, time);
+            startActivity(intent);
+        }
+    }
+}
---
+++ Lab3.4/app/src/main/java/com/example/egg_timer/TimerActivity.java
@@ -0,0 +1,38 @@
+package com.example.egg_timer;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class TimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+        final TextView timeCount = findViewById(R.id.timeCounter);
+        Intent intent = getIntent();
+        String time = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
+
+        new CountDownTimer(Integer.parseInt(time)*1000,1000) {
+            public void onTick(long millisUntilFinished){
+                timeCount.setText("" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                mp.start();
+                finish();
+            }
+        }.start();
+    }
+}
+
