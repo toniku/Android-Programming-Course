722a7ce217dcdae198bf89e201762cb8ac16f2d0
Toni <K>
2019-02-15 15:32:09 +0200

    UI

---
+++ Lab2.1/app/src/main/java/com/example/toni_/ui_hierarchies/UI_Hierarchies.java
@@ -0,0 +1,67 @@
+package com.example.toni_.ui_hierarchies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UI_Hierarchies extends AppCompatActivity implements View.OnClickListener {
+
+    boolean containsText = false;
+    ArrayList<String> countries = new ArrayList<>();
+    final String[] COUNTRIES = new String[]{ "Afghanistan", "Albania" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui__hierarchies);
+        findViewById(R.id.add_button).setOnClickListener(this);
+        findViewById(R.id.edit_button).setOnClickListener(this);
+        findViewById(R.id.remove_button).setOnClickListener(this);
+        countries.addAll(Arrays.asList(COUNTRIES));
+        printCountries();
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.add_button) {
+            addCountry();
+        }
+        else if (view.getId() == R.id.remove_button) {
+            removeCountry();
+        }
+    }
+
+    public void printCountries(){
+        ListView myListView = (ListView) findViewById(R.id.country_list_view);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, countries);myListView.setAdapter(adapter);
+    }
+
+    public String getTextFieldText(){
+        EditText editor = findViewById(R.id.text_editor);
+        String text = editor.getText().toString();
+        editor.setText(null);
+        return text;
+    }
+
+    public void addCountry(){
+        String name = getTextFieldText();
+        if (name != null && !name.isEmpty())
+        {
+            countries.add(name);
+            printCountries();
+        }
+    }
+
+    public void removeCountry(){
+        String name = getTextFieldText();
+        countries.remove(name);
+        printCountries();
+    }
+}
