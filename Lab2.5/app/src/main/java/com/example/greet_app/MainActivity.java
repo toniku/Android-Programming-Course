9e6d88cbea806f8e3d4e9052f880404e99b57523
Toni <>
2019-03-01 00:49:45 +0200

    Lab 2.5

--- Lab2.5/app/src/main/java/com/example/greet_app/MainActivity.java
+++ Lab2.5/app/src/main/java/com/example/greet_app/MainActivity.java
@@ -18,30 +18,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.sverige_button).setOnClickListener(this);
         findViewById(R.id.suomeksi_button).setOnClickListener(this);
         findViewById(R.id.surprise_button).setOnClickListener(this);
-
-        final TextView welcome_message = findViewById(R.id.hello_text);
-        final EditText name = findViewById(R.id.text_name);
     }
 
     @Override
-    public void onClick(View view){
+    public void onClick(View view) {
 
         TextView welcome_message = findViewById(R.id.hello_text);
         EditText name = findViewById(R.id.text_name);
 
-        if (view.getId() == R.id.english_button){
+        if (view.getId() == R.id.english_button) {
             welcome_message.setText("Hello " + name.getText().toString());
-        }
-
-        else if (view.getId() == R.id.sverige_button){
+        } else if (view.getId() == R.id.sverige_button) {
             welcome_message.setText("Hejjsan " + name.getText().toString());
-        }
-
-        else if (view.getId() == R.id.suomeksi_button){
+        } else if (view.getId() == R.id.suomeksi_button) {
             welcome_message.setText("Hei " + name.getText().toString());
-        }
-
-        else if (view.getId() == R.id.surprise_button){
+        } else if (view.getId() == R.id.surprise_button) {
             welcome_message.setText("Tere " + name.getText().toString());
         }
     }
