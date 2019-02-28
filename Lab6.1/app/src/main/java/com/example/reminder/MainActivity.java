6ad579a846d4ad4a1e6dda1b1276209ebe5c00e8
Toni <K>
2019-02-28 20:26:59 +0200

    Lab 6.1

--- Lab6.1/app/src/main/java/com/example/reminder/MainActivity.java
+++ Lab6.1/app/src/main/java/com/example/reminder/MainActivity.java
@@ -2,96 +2,107 @@ package com.example.reminder;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
-import java.sql.Time;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final String SHARED_PREF_FILE = "MyApp";
-    static final String SHARED_PREF_EDITOR_TEXT_KEY = "textInTheEditor";
-    private int notificationId = 1;
-
-    EditText reminderText = null;
+    EditText reminderText;
+    TimePicker timePicker;
+    ReminderData reminderData;
+    private int notificationId = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TimePicker picker = (TimePicker) findViewById(R.id.timePicker);
-        picker.setIs24HourView(true);
+        reminderData = new ReminderData(this);
+        timePicker = findViewById(R.id.timePicker);
         reminderText = findViewById(R.id.reminder_text);
+        timePicker.setIs24HourView(true);
 
+        // When user sets reminder, we create an pending intent which is sent to the ReminderBroadcastReceiver
         findViewById(R.id.done_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                EditText editText = findViewById(R.id.reminder_text);
-                TimePicker timePicker = findViewById(R.id.timePicker);
-
+            public void onClick(View view) {
                 // Set notificationId & text.
-                Intent intent = new Intent(MainActivity.this, ReminderBroadcastReceiver.class);
-                intent.putExtra("notificationId", notificationId);
-                intent.putExtra("todo", editText.getText().toString());
+                Intent intent = new Intent(getApplicationContext(), ReminderBroadcastReceiver.class);
+                intent.putExtra("notificationText", reminderData.getNotificationText());
 
-                // getBroadcast(context, requestCode, intent, flags)
-                PendingIntent alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0,
-                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                // Create pending intent with time given
+                PendingIntent alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), notificationId,
+                        intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+                // Create AlarmManager which is sent with pending intent
                 AlarmManager alarm = (AlarmManager) getSystemService(ALARM_SERVICE);
-
-                int hour = timePicker.getCurrentHour();
-                int minute = timePicker.getCurrentMinute();
-
-                // Create time.
+                // Create time when notification will fire
                 Calendar startTime = Calendar.getInstance();
-                startTime.set(Calendar.HOUR_OF_DAY, hour);
-                startTime.set(Calendar.MINUTE, minute);
+                startTime.set(Calendar.DAY_OF_WEEK, startTime.get(Calendar.DAY_OF_WEEK));
+                startTime.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+                startTime.set(Calendar.MINUTE, timePicker.getMinute());
                 startTime.set(Calendar.SECOND, 0);
+
                 long alarmStartTime = startTime.getTimeInMillis();
 
-                // Set alarm.
-                // set(type, milliseconds, intent)
-                alarm.set(AlarmManager.RTC_WAKEUP, alarmStartTime, alarmIntent);
 
-                Log.d("LAB6.1", "MainActivity, onClick-METHOD");
+                /* If reminder time is smaller than current time, start notification on next day, still in progress */
+
+                /*
+                if (startTime.getTimeInMillis() < System.currentTimeMillis()) {
+                    startTime.set(Calendar.DAY_OF_WEEK, startTime.get(Calendar.DAY_OF_WEEK + 1));
+                    alarmStartTime = startTime.getTimeInMillis();
+                }
+                else
+                {
+                    alarmStartTime = startTime.getTimeInMillis();
+                }
+                */
+
+
+                // Set alarm with (type, milliseconds, intent)
+                // With RTC_WAKEUP we wake the device up and deliver the pending intent
+                alarm.set(AlarmManager.RTC_WAKEUP, alarmStartTime, alarmIntent);
+                saveData();
 
             }
         });
     }
 
+    // Save data when app stops
     @Override
     protected void onStop() {
         super.onStop();
-        String text = reminderText.getText().toString();
-        SharedPreferences sharedPreferences = getPref();
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(SHARED_PREF_EDITOR_TEXT_KEY, text);
-        editor.commit();
+        saveData();
     }
 
+    // Load data when app resumes
     @Override
     protected void onResume() {
         super.onResume();
-        SharedPreferences sharedPreferences = getPref();
-        String previousText = sharedPreferences.getString(SHARED_PREF_EDITOR_TEXT_KEY, null);
-        reminderText.setText(previousText);
+        loadData();
     }
 
-    protected SharedPreferences getPref() {
-        return getSharedPreferences(SHARED_PREF_FILE, MODE_PRIVATE);
+    // For data saving we use ReminderData class
+    // ReminderData class has a function that saves all the information to SharedPreferences
+    private void saveData() {
+        // Save data to ReminderData class
+        reminderData.setMinutes(timePicker.getMinute());
+        reminderData.setDataHours(timePicker.getHour());
+        reminderData.setNotificationText(reminderText.getText().toString());
+        reminderData.saveDataToSharedPreferences();
     }
 
-    private void PickTime() {
+    private void loadData() {
+        // Load all the data from ReminderData class
+        reminderData.loadDataFromSharedPreferences();
+        reminderText.setText(reminderData.getNotificationText());
+        timePicker.setMinute(reminderData.getMinutes());
+        timePicker.setHour(reminderData.getHours());
     }
 }
--- Lab6.1/app/src/main/java/com/example/reminder/ReminderBroadcastReceiver.java
+++ Lab6.1/app/src/main/java/com/example/reminder/ReminderBroadcastReceiver.java
@@ -1,66 +1,37 @@
 package com.example.reminder;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.util.Log;
 
+// BroadcastReceiver is a base class for code that receives and handles broadcast intents by Context.sendBroadcast(Intent)
 public class ReminderBroadcastReceiver extends BroadcastReceiver {
+
+    int notificationId = 100;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Log.d("LAB6.1", "BroadcastReceiver - onReceive-METHOD");
-
-        private void createNotificationChannel() {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                CharSequence name = getString(R.string.channel_name);
-                String description = getString(R.string.channel_description);
-                int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-                channel.setDescription(description);
-                // Register the channel with the system; you can't change the importance
-                // or other notification behaviors after this
-                NotificationManager notificationManager = getSystemService(NotificationManager.class);
-                notificationManager.createNotificationChannel(channel);
-            }
-        }
-
-        // Get id & message from intent.
-        int notificationId = intent.getIntExtra("notificationId", 0);
-        String message = intent.getStringExtra("todo");
-
-        // When notification is tapped, call MainActivity.
+        // Create intent that is sent when user taps the notification
         Intent mainIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
-        NotificationManager myNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, notificationId, mainIntent, 0);
 
+        // Notification content and channel set
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, ReminderApplication.CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("Your Reminder!")
+                .setContentText(intent.getStringExtra("notificationText"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT) // On Android 7.1 and lower determines how intrusive the notification is
+                .setContentIntent(pendingIntent) // When notification is tapped, call MainActivity.
+                .setAutoCancel(true); // Notification is automatically removed when tapped
 
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
-
-        // Prepare notification.
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle("It's Time!")
-                .setContentText(message)
-                .setWhen(System.currentTimeMillis())
-                .setAutoCancel(true)
-                .setContentIntent(contentIntent)
-                .setPriority(Notification.PRIORITY_MAX)
-                .setDefaults(Notification.DEFAULT_ALL);
-
-        // Notify
-        myNotificationManager.notify(notificationId, builder.build());
-
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, builder.build());
     }
-
 }
